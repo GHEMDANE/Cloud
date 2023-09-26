fdb5e3ff413d4d9bdbf483ab235a5d066c3edd5f
Ghemdane <cghemdane1@myges.fr>
2023-09-26 14:52:41 +0200

    commit

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  '', cloud_xgoa // Database name
+  '',clouduser // User
+  '', rPFT1oNxllecrqR9rD04rvDQ5CoOkqlD // Password
   {
-    host: '', // Host
+    host: '', dpg-ck9d0fv0vg2c73d10gag-a // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
